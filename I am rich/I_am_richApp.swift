836660b6a335e0336d2a464fc793a583abf9877a
Eric Tan <83531295+erictan16@users.noreply.github.com>
2023-06-14 16:16:37 +1000

    Initial Commit

---
+++ I am rich/I_am_richApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_richApp.swift
+//  I am rich
+//
+//  Created by Eric Tan on 14/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct I_am_richApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
